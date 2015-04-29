2a8feb42e272e79b192f66c3bf852aaa78040f98
Dennis Sänger <mail@dennis.io>
2015-04-29 08:24:46 +0200

    style(contextmenu): Overhaul js source file style

--- contextmenu.js
+++ contextmenu.js
@@ -1,81 +1,90 @@
-var app = angular.module('io.dennis.contextmenu', []);
+'use strict';
+
 var pointerEvents = 'contextmenu';
 
-app.directive('contextmenu', ['$parse', function( $parse ) {
-  
-  var $window = angular.element( window );
+angular.module('io.dennis.contextmenu', [])
+  .directive('contextmenu', Contextmenu)
+  .directive('contextmenuContainer', ContextmenuContainer)
+  .directive('contextmenuItem', ContextmenuItem);
+
+Contextmenu.$inject = ['$parse'];
+function Contextmenu($parse) {
 
   return {
     restrict: 'A',
     scope: false,
-    controller: ['$scope', '$attrs', function( $scope, $attrs ) {
-      var ctrl = {};
-
-      ctrl.$element = null;
-      ctrl.$isOpen = false;
-
-      ctrl.close = function() {
-        ctrl.$element.toggleClass( 'open', false );
-        ctrl.$isOpen = false;
-      };
-
-      ctrl.open = function( item, event ) {
-
-        ctrl.$item = item;
-
-        var top = event.pageY - $window.scrollTop();
-        ctrl.$element.css( 'left', event.pageX + 'px' );
-        ctrl.$element.css( 'top', top + 'px' );
-        ctrl.$element.toggleClass( 'open', true );
-        ctrl.$isOpen = true;
-
-        return ctrl.isOpen;
-      };
-
-      var closeEvents = pointerEvents + ' ' + 'click';
-      $window.on( closeEvents, function() {
-        if ( ctrl.$isOpen ) {
-          ctrl.close();
-        }
-      });
-
-      return ctrl;
-    }],
-    link: function( $scope, $element, $attrs, $ctrl ) {
+    controller: ContextmenuCtrl,
+    link: function($scope, $element, $attrs, $ctrl) {
       $ctrl.$element = $element;
-      $parse( $attrs.contextmenu ).assign( $scope, $ctrl );
+      $parse($attrs.contextmenu).assign($scope, $ctrl);
     }
-  }
-}]);
+  };
+}
 
-app.directive('contextmenuContainer', ['$parse', function( $parse ) {
+ContextmenuCtrl.$inject = ['$window'];
+function ContextmenuCtrl(window) {
+  var ctrl = this;
+  var $window = angular.element(window);
+
+  ctrl.$element = null;
+  ctrl.$isOpen = false;
+
+  ctrl.close = close;
+  ctrl.open = open;
+
+  function close() {
+    ctrl.$element.toggleClass('open', false);
+    ctrl.$isOpen = false;
+  }
+
+  function open(item, event) {
+
+    ctrl.$item = item;
+
+    var top = event.pageY - $window.scrollTop();
+    ctrl.$element.css('left', event.pageX + 'px');
+    ctrl.$element.css('top', top + 'px');
+    ctrl.$element.toggleClass('open', true);
+    ctrl.$isOpen = true;
+
+    return ctrl.isOpen;
+  }
+
+  var closeEvents = pointerEvents + ' ' + 'click';
+  $window.on(closeEvents, function() {
+    if (ctrl.$isOpen) {
+      ctrl.close();
+    }
+  });
+}
+
+ContextmenuContainer.$inject = ['$parse'];
+function ContextmenuContainer($parse) {
   return {
     restrict: 'A',
     scope: false,
-    controller: ['$scope', '$attrs', function( $scope, $attrs ) {
-      return $parse( $attrs.contextmenuContainer )( $scope );
+    controller: ['$scope', '$attrs', function($scope, $attrs) {
+      return $parse($attrs.contextmenuContainer)($scope);
     }],
-//    link: function( $scope, $element, $attrs ) {
-//      
-//    }
-  }
-}]);
+  };
+}
 
-app.directive('contextmenuItem', [function() {
+ContextmenuItem.$inject = [];
+function ContextmenuItem() {
   return {
     restrict: 'A',
     require: '^contextmenuContainer',
     scope: false,
-    link: function( $scope, $element, $attrs, contextMenuCtrl ) {
+    link: function($scope, $element, $attrs, contextMenuCtrl) {
 
-      var iam = $scope[( $attrs.contextmenuItem )];
-      $element.on( pointerEvents, function( event ) {
+      var iam = $scope[($attrs.contextmenuItem)];
+      $element.on(pointerEvents, function(event) {
         $scope.$apply(function() {
-          contextMenuCtrl.open( iam, event );
+          contextMenuCtrl.open(iam, event);
         });
         event.stopPropagation();
         return false;
       });
     }
-  }
-}]);
+  };
+}
